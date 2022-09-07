63ce7d0dec20096bbb79f6c4c73fa0a06f0e7c08
Jorge Jara <jorgejarainostroza@gmail.com>
2022-09-07 17:53:26 -0400

    Change -t argument to -th

--- src/main.cpp
+++ src/main.cpp
@@ -8,15 +8,18 @@
 #include "timer.h"
 
 const std::vector<Arguments::ArgDefinition> args_list{
-    {'i', Arguments::ArgDefinition::INT},
-    {'t', Arguments::ArgDefinition::DOUBLE},
+    {'i', Arguments::ArgDefinition::STRING},
+    {'t', Arguments::ArgDefinition::INT},
+    {'h',                                // Treat -th as -t -h
+     Arguments::ArgDefinition::DOUBLE},  // (the threshold value will actually
+                                         // be contained in the -h argument)
 };
 
 int main(int argc, char* argv[]) {
     Arguments args(args_list, argc, argv);
 
-    const auto instance_arg = args.get<int>('i');
-    const auto threshold_arg = args.get<double>('t');
+    const auto instance_arg = args.get<std::string>('i');
+    const auto threshold_arg = args.get<double>('h');
 
     if (!instance_arg || !threshold_arg) {
         return 1;
@@ -25,7 +28,7 @@ int main(int argc, char* argv[]) {
     const auto instance = instance_arg.value();
     const auto threshold = threshold_arg.value();
 
-    ffmsp::greedy({"AATG", "CTGA", "AAAA", "AATA"}, instance);
+    ffmsp::greedy({"AATG", "CTGA", "AAAA", "AATA"}, threshold);
 
     return 0;
 }
