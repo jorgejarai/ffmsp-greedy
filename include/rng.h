7b29c6d1e7466108fff25ac5260b6f0b83b7c6bb
Jorge Jara <jorgejarainostroza@gmail.com>
2022-09-10 11:03:55 -0400

    Implementar RNG::rand_choose

--- include/rng.h
+++ include/rng.h
@@ -24,6 +24,17 @@ public:
     int rand_int(int a, int b);
     double rand_real(double a, double b);
     bool rand_bool();
+
+    template <typename T>
+    typename T::value_type rand_choose(const T& options) {
+        if (options.size() == 1) {
+            return options[0];
+        }
+
+        auto next_candidate_index = rand_int(0, options.size() - 1);
+
+        return options[next_candidate_index];
+    }
 };
 
 #endif  // RNG_H
--- src/rng.cpp
+++ src/rng.cpp
@@ -6,19 +6,19 @@ RNG::RNG() : m_gen(m_rd()) {
 }
 
 int RNG::rand_int(int a, int b) {
-    std::uniform_int_distribution<> dist(a, b);
+    std::uniform_int_distribution<int> dist(a, b);
 
     return dist(m_gen);
 }
 
 double RNG::rand_real(double a, double b) {
-    std::uniform_real_distribution<> dist(a, b);
+    std::uniform_real_distribution<double> dist(a, b);
 
     return dist(m_gen);
 }
 
 bool RNG::rand_bool() {
-    std::uniform_int_distribution<> dist(0, 1);
+    std::uniform_int_distribution<int> dist(0, 1);
 
     return dist(m_gen) != 0;
 }
